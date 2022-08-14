83e751da42fd1653a0b8532b92af3770be745d07
40034243 <87721818+40034243@users.noreply.github.com>
2022-08-14 13:01:29 -0400

    Update automated_flips.py

--- automated_flips.py
+++ automated_flips.py
@@ -1,26 +1,55 @@
 from decimal import Decimal
 import random
-
+loss_rate = 0
+count = 0
 total = 0
 
-betting_amount = 20
+betting_amount = 5
+amount_of_rounds_tested = 100
 
-while True:
-        new_balance = 100
-        flips = 0
-        while flips < 1000:
-            if random.randint(0, 100) <= 60:
-                new_balance = Decimal(Decimal(new_balance) + Decimal(betting_amount))
-            else:
-                new_balance = Decimal(Decimal(new_balance) - Decimal(betting_amount))
-            
-            flips += 1
-            
-            if new_balance <= 0:
-                print("BANKRUPT! after", flips, "flips")
-                bankruptcy_rate = 1 / (total + 1) * 100
-                print("Bankruptcy rate:", bankruptcy_rate, "%")
-                exit()
-            
-        total += 1     
-        print(new_balance)
+
+for i in range(amount_of_rounds_tested):
+    new_balance = 100
+    flips = 0
+    while flips < 100000:
+        if random.randint(0, 100) <= 51:
+            new_balance = Decimal(Decimal(new_balance) + Decimal(betting_amount))
+        else:
+            new_balance = Decimal(Decimal(new_balance) - Decimal(betting_amount))
+        
+        if new_balance <= 0:
+            break
+        flips += 1
+        
+    print(new_balance)
+    if new_balance < 100:
+        loss_rate += 1
+        
+print("Bankruptcy rate:", loss_rate / amount_of_rounds_tested * 100,"%")
+        
+
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+
+
+    
