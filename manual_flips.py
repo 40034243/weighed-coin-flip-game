b3123df4ab243dac147f7fee4f65cf0d6bc8ac98
40034243 <87721818+40034243@users.noreply.github.com>
2022-08-13 23:18:14 -0400

    Create manual_flips.py

---
+++ manual_flips.py
@@ -0,0 +1,61 @@
+from decimal import Decimal
+import random
+
+
+balance = 25
+
+
+for i in range(1):
+    print("Please input H or T: ")
+    x = input()
+    print("Please input bet amount: ")
+    y = input()
+    if x == "H":
+        if random.randint(0, 100) <= 60:
+            print("You won!")
+            new_balance = Decimal(Decimal(balance) + Decimal(y))
+            print("New Balance: ")
+            print(new_balance)
+
+    else:
+        print("You lost.")
+        new_balance = Decimal(Decimal(balance) - Decimal(y))
+        print("New Balance: ")
+        print(new_balance)
+        
+    if balance <= 0:
+            exit()
+            
+    while True:
+        print("Please input H or T: ")
+        x = input()
+        print("Please input bet amount: ")
+        y = input()
+        if x == "H":
+            if random.randint(0, 100) <= 60:
+                print("You won!")
+                new_balance = Decimal(Decimal(new_balance) + Decimal(y))
+                print("New Balance: ")
+                print(new_balance)
+            else:
+                print("You lost.")
+                new_balance = Decimal(Decimal(new_balance) - Decimal(y))
+                print("New Balance: ")
+                print(new_balance)
+                
+        if x == "T":
+            if random.randint(0, 100) <= 40:
+                print("You won!")
+                new_balance = Decimal(Decimal(new_balance) + Decimal(y))
+                print("New Balance: ")
+                print(new_balance)
+            else:
+                print("You lost.")
+                new_balance = Decimal(Decimal(new_balance) - Decimal(y))
+                print("New Balance: ")
+                print(new_balance)
+                
+        if new_balance <= 0:
+            exit()
+            
+            
