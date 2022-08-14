246c2e55993fb49f58252c350db7a08f05c68a25
40034243 <87721818+40034243@users.noreply.github.com>
2022-08-14 18:01:02 -0400

    Update automated_flips.py

--- automated_flips.py
+++ automated_flips.py
@@ -1,57 +1,32 @@
 from decimal import Decimal
+
 import random
+
 loss_rate = 0
 count = 0
 total = 0
 
-betting_amount = 100
-amount_of_rounds_tested = 365
+new_balance = 100
+betting_amount = new_balance * .10000000000000009
+amount_of_rounds_tested = 1
 
 for i in range(amount_of_rounds_tested):
     new_balance = 100
     flips = 0
     while flips < 100000:
-        if random.randint(0, 100) <= 51:
+        if random.randint(0, 100) <= 55:
             new_balance = Decimal(Decimal(new_balance) + Decimal(betting_amount))
         else:
             new_balance = Decimal(Decimal(new_balance) - Decimal(betting_amount))
         
+        flips += 1
         if new_balance <= 0:
             break
-        flips += 1
         
     print(new_balance)
     
     if new_balance < 100:
         loss_rate += 1
-        
 
-print(new_balance)
 print("Loss rate:", loss_rate / amount_of_rounds_tested * 100,"%")
-        
-
-    
-    
-    
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    
-
-
     
